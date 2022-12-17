ed06810b87b2bf08bff0f7c53af7a703950596bf
JasonPY987 <corey.business.987@outlook.com>
2022-12-17 16:50:58 +0400

    second commit added the listing model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,18 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class catagory(models.Model):
+    NameCatagory = models.CharField(max_length=50)
+    
+
+class Listing(models.Model):
+    ProductName = models.CharField(max_length=50)
+    date = models.DateField()
+    description = models.CharField(max_length=300)
+    imageURL = models.CharField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    isActive = models.BooleanField(default= True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name="user")
+    catagory = models.ForeignKey(catagory, on_delete=models.CASCADE, blank=True, related_name="catagory")
+    
